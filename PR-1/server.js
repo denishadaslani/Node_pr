2c853dbf3af386e28a067e2c077be3e696bf1719
denishadaslani <denishadashlani@gmail.com>
2025-12-01 12:21:04 +0530

    1-12-25

---
+++ PR-1/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(
+    (req, res) => {
+
+        let filepath = "";
+        
+        switch (req.url) {
+            case "/":
+                filepath = "../index.html";
+                break;
+            case "/about":
+                filepath = "../about.html";
+                break;
+            case "/collection":
+                filepath = "../collection.html";
+                break;
+            case "/contact":
+                filepath = "../contact.html";
+                break;
+
+            default:
+                filepath = "../not-found.html";
+                break;
+        }
+
+        let data = fs.readFileSync(filepath, 'utf-8');
+        res.end(data);
+    }
+);
+
+let err;
+
+server.listen(8000, () => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Server is running on port 8000");
+    }
+});
